ebce203bfedbf7fa2bfe79aadfc14144a4ac0fc7
iewihc <ji3cp4su3x83@gmail.com>
2021-08-21 18:46:56 +0800

    hi

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-python3 /line_notify.py \
-  --token "$INPUT_TOKEN" \
-  --message "$INPUT_MESSAGE" \
-  --image-url "$INPUT_IMAGE_URL" \
-  --sticker-id "$INPUT_STICKER_ID" \
-  --package-id "$INPUT_PACKAGE_ID" \
-  --image-file "$INPUT_IMAGE_FILE"
